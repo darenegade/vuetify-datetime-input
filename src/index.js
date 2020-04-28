066f173f4adc059858f2699ec73d4092343ff9b5
René Zarwel <darenegade@gmx.de>
2020-04-28 22:24:19 +0200

    format

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import VuetifyDatetimeInput from "./VuetifyDatetimeInput.vue";
 
-
 Vue.component("v-datetime-input", VuetifyDatetimeInput);
 
 export default VuetifyDatetimeInput;
